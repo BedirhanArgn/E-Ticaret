c99090ef1fd7a6755725e3a1e73fcab1bfda4161
BedirhanArgn <argunbedir@gmail.com>
2020-07-15 00:55:22 +0300

    Add product from Admin

--- E-Ticaret.Webui/Controllers/AdminController.cs
+++ E-Ticaret.Webui/Controllers/AdminController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using E_ticaret.business.Abstract;
+using E_ticaret.Entity;
 using E_Ticaret.ViewModel;
+using E_Ticaret.Webui.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace E_Ticaret.Webui.Controllers
 {
@@ -25,5 +28,29 @@ namespace E_Ticaret.Webui.Controllers
 
             }) ;
         }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateProduct(ProductView product)
+        {
+            var entity = new Product()
+            {
+                Name = product.Name,
+                Description = product.Description,
+                Url = product.Url,
+                Price = product.Price,
+                ImageUrl = product.ImageUrl
+            };
+
+            _productService.Create(entity);
+            return RedirectToAction("ProductList");
+        }
+
     }
 }
---
+++ E-Ticaret.Webui/Models/ProductView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Ticaret.Webui.Models
+{
+    public class ProductView
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+
+        public double? Price { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public String ImageUrl { get; set; }
+        public bool IsHome { get; set; } //Eğer home page de göster dersek göstereceğiz.
+        public bool IsApproved { get; set; }
+
+
+    }
+}
