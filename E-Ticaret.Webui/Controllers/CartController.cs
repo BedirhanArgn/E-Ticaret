ed1c7491b2a08c661557818cbf302aabaea52b13
BedirhanArgn <argunbedir@gmail.com>
2020-08-10 01:00:17 +0300

    Added adding to card button

---
+++ E-Ticaret.Webui/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Ticaret.Webui.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCard()
+        {
+            return View();
+        }
+    }
+}
